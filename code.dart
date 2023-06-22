90e882523079da4f9dd518fb545ee817098a82f1
Robert Nystrom <4302952+robustzm@users.noreply.github.com>
2023-06-22 13:52:34 -0700

    Format using experimental formatter.

--- code.dart
+++ code.dart
@@ -1,26 +1,35 @@
 assignment() {
-  TextSpan _textSpan =
-      const TextSpan(text: 'Welcome to the Flutter text fuzzer.');
+  TextSpan _textSpan = const TextSpan(
+    text: 'Welcome to the Flutter text fuzzer.',
+  );
 }
 
-typedef ProcessResult ProcessRunner(String executable, List<String> arguments,
-    {String workingDirectory,
-    Map<String, String> environment,
-    bool includeParentEnvironment,
-    bool runInShell,
-    Encoding stdoutEncoding,
-    Encoding stderrEncoding});
+typedef ProcessResult ProcessRunner(
+  String executable,
+  List<String> arguments, {
+  String workingDirectory,
+  Map<String, String> environment,
+  bool includeParentEnvironment,
+  bool runInShell,
+  Encoding stdoutEncoding,
+  Encoding stderrEncoding,
+});
 
 unbracedIf() {
   if (_random.nextInt(10) > 0) return text;
+  // No change around unbraced ifs. I'd want to gather data on what users want
+  // here before we make a choice. My impression is that multi-line unbraced
+  // ifs are generally unpopular.
 }
 
 class Foo extends A {
   Foo({
     Bar bar,
     Baz baz,
-  })  : assert(bar != null), // Comment here to force split.
-        super(baz);
+  }) : assert(bar != null), // Comment here to force split.
+       super(baz);
+  // Now instead of spacing the first line to align with the others, it spaces
+  // the others to align with the first.
 }
 
 class RenderUnconstrainedBox extends RenderBox
@@ -28,20 +37,32 @@ class RenderUnconstrainedBox extends RenderBox
         RenderObjectWithChildMixin<RenderBox>,
         RenderProxyBoxMixin,
         DebugOverflowIndicatorMixin {
+  // The formatter does't generally do alignment, though I did end up doing
+  // alignment for constructor initializers. We could explore doing that here
+  // too. But I kind of prefer not doing that here because if a class has
+  // long `with` and `implements` clauses, then the style here ensures that
+  // the two lists of types align with each other, which I think looks nice.
   RenderUnconstrainedBox();
 }
 
 examples() async {
-  return runZoned(() => new Future<T>(f),
-      zoneSpecification: new ZoneSpecification(
-          print: (Zone self, ZoneDelegate parent, Zone zone, String line) =>
-              _outputStream.writeln(line)));
+  return runZoned(
+    () => new Future<T>(f),
+    zoneSpecification: new ZoneSpecification(
+      print: (
+        Zone self,
+        ZoneDelegate parent,
+        Zone zone,
+        String line,
+      ) => _outputStream.writeln(line),
+    ),
+  );
 
   foo('12345678901234567890123456789012345678901234567890')
       .bar('12345678901234567890123456789012345678901234567890')
       .baz(() {
-    quux();
-  });
+        quux();
+      });
 
   return new Scaffold(
     appBar: new AppBar(
@@ -50,19 +71,18 @@ examples() async {
       title: new Text(widget.title),
     ),
     body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new SizedBox(
-      height: 200.0,
-      width: 200.0,
-      child: new FittedBox(
-        fit: BoxFit.scaleDown,
-        child: const Text(
-          'Blah blah blah blah blah',
-          style: const TextStyle(),
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+      child: new SizedBox(
+        height: 200.0,
+        width: 200.0,
+        child: new FittedBox(
+          fit: BoxFit.scaleDown,
+          child:
+              const Text('Blah blah blah blah blah', style: const TextStyle()),
         ),
       ),
-    )),
+    ),
   );
 
   return runZoned(
@@ -74,21 +94,30 @@ examples() async {
     ),
   );
 
-  return FooBarFooBarFooBarFooBar.convert(await baz.invokeFooBar<String,
-          String>(
+  return FooBarFooBarFooBarFooBar.convert(
+    await baz.invokeFooBar<String, String>(
       'FooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBar',
-      'FooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBar'));
+      'FooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBarFooBar',
+    ),
+  );
 
-  double height = math.max(upButton!.getMaxIntrinsicHeight(double.infinity),
-          downButton!.getMaxIntrinsicHeight(double.infinity)) *
+  double height = math.max(
+        upButton!.getMaxIntrinsicHeight(double.infinity),
+        downButton!.getMaxIntrinsicHeight(double.infinity),
+      ) *
       2;
+  // This still looks pretty weird. In general, the formatter tries to split
+  // infix expressions if either operand gets split so that the infix operation
+  // itself doesn't get buried in one of its operands. That looks weird in
+  // edge cases like this, but generally works well.
 
   expect(
-      await const BasicMessageChannel<Object>(
-        'dev.flutter.pigeon.Api.initialize',
-        StandardMessageCodec(),
-      ).send(null),
-      isEmpty);
+    await const BasicMessageChannel<Object>(
+      'dev.flutter.pigeon.Api.initialize',
+      StandardMessageCodec(),
+    ).send(null),
+    isEmpty,
+  );
 
   // Without comment:
   print(<int>[
